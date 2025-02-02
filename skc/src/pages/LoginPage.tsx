edaaf5abfed3f2e9ae8d57068c1c8070cb842d05
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2025-02-02 19:10:41 +0530

    Update LoginPage.tsx

--- skc/src/pages/LoginPage.tsx
+++ skc/src/pages/LoginPage.tsx
@@ -55,13 +55,13 @@ const LoginPage = () => {
           </div>
 
           <div>
-            <a
-              href="/chat"
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Log in
-            </a>
+            <Link
+                to="/chat"
+                type={"submit"}
+                className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+              >
+                Log in
+              </Link>
           </div>
         </form>
         <div className="text-center">
