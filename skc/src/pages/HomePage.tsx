7186a12cda77078c28100e1e20eb8cb16c628be3
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2025-02-02 18:57:32 +0530

    Update HomePage.tsx

--- skc/src/pages/HomePage.tsx
+++ skc/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { BookOpen, Users, Award } from "lucide-react"
-import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const HomePage = () => {
   return (
